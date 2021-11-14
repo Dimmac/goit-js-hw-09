cfddb09f75f8f599ee6c0930e1fa1c54be1a92cf
Dimmac <84150134+Dimmac@users.noreply.github.com>
2021-11-14 09:06:49 +0200

    without step

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,70 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const delay = Number(event.target.elements.delay.value);
+  const step = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
+
+  for (let i = 0; i <= amount; i += 1) {
+    setTimeout(() => {
+      createPromise(i + 1, delay)
+        .then(({ position, delay }) => {
+          notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }),
+        delay;
+    });
+    // delay += step;
   }
 }
 
-const refs = {
-  delay: document.querySelector('.delay'),
-  step: document.querySelector('.step'),
-  amount: document.querySelector('.amount'),
-  submit: document.querySelector('.submit'),
-};
-console.log(refs.delay);
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+refs.form.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   console.log('це значення, введене в поле delay', event.currentTarget.elements.delay.value);
+//   console.log('це значення, введене в поле step', event.currentTarget.elements.step.value);
+//   console.log('це значення, введене в поле amount', event.currentTarget.elements.amount.value);
+//   // const {
+//   //   elements: { delay, step, amount },
+//   // } = event.currentTarget;
+
+//   const {
+//     elements: { delay, step, amount },
+//   } = event.currentTarget;
+//   // console.log(elements);
+//   console.log(
+//     `значення ДІЛЕЙ: ${delay.value}, значення СТЕП: ${step.value}, значення ЕМАУНТ: ${amount.value}`,
+//   );
+// }
+// console.log( event.currentTarget);
+// console.log(refs.delay);
+// console.log(refs.step);
+// refs.delay.addEventListener('input', () => {
+//   console.log(refs.delay);
+// });
 
 // refs.submit.addEventListener('click', () => {
 //   function();
